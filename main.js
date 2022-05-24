52e7138afe3348fd21f25d690f7a2faf81660ad3
linneavisser <anna56c1@stud.kea.dk>
2022-05-24 20:58:42 +0200

    product page first commit

--- main.js
+++ main.js
@@ -5,3 +5,20 @@ const navbar = document.querySelector(".navbar");
 menuIcon.addEventListener("click", () => {
   navbar.classList.toggle("change");
 });
+
+// PRODUCT VIEW PAGE //
+
+// toggle & collapsible //
+function toggleArtistInfo() {
+  var info = document.querySelector(".toggle-info");
+  var icon = document.querySelector("img.product-arrow");
+  if (info.style.display === "block") {
+    info.style.display = "none";
+    icon.src = "svg/arrowdown.svg";
+  } else {
+    info.style.display = "block";
+    icon.src = "svg/arrowleft.svg";
+  }
+}
+const arrowToggleArtist = document.querySelector(".toggle-info-artist");
+arrowToggleArtist.addEventListener("click", toggleArtistInfo);
