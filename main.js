33dc2dc9f05e10165338620b269e22f0d9cb8ec3
Thordis Kara Valsdottir <thor9706@stud.kea.dk>
2022-05-24 18:44:53 +0200

    JS fix

--- main.js
+++ main.js
@@ -64,20 +64,17 @@ function showProducts(productArray) {
 }
 
 // function showSingleProduct(singleproduct) {
-//   console.log(singlebag);
-//   document.querySelector(".bagName").textContent = singlebag.bagname;
-//   document.querySelector(".bagPrice span").textContent = singlebag.price;
+//   console.log(singleproduct);
+//   document.querySelector(".name").textContent = singleproduct.title.rendered;
+//   document.querySelector(".price span").textContent = singleproduct.price;
 
-//   document.querySelector(".description").textContent = singlebag.description;
-//   document.querySelector(".bagColor").textContent = singlebag.color;
-//   document.querySelector(".toggleInner .bagMaterial span").textContent =
-//     singlebag.material;
-//   document.querySelector(".toggleInner .bagDimensions span").textContent =
-//     singlebag.dimensions;
-//   document.querySelector(".toggleInner .bagStrap span").textContent =
-//     singlebag.straplength;
-//   document.querySelector(".bagImg").src =
-//     singlebag._embedded[
+//   document.querySelector(".description").textContent = singleproduct.description;
+//   document.querySelector(".color").textContent = singleproduct.color;
+//   document.querySelector(".stock").textContent = singleproduct.stock;
+//   document.querySelector(".designerinfo").textContent =
+//     singleproduct.designer_info;
+//   document.querySelector(".img").src =
+//     singleproduct._embedded[
 //       "wp:featuredmedia"
-//     ][0].media_details.sizes.large.source_url;
+//     ][0].media_details.sizes.medium_large.source_url;
 // }
