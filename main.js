14fa1b7628fc435b32362d7b658423b0be017770
Thordis Kara Valsdottir <thor9706@stud.kea.dk>
2022-05-24 18:13:18 +0200

    featured box

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ const navbar = document.querySelector(".navbar");
 menuIcon.addEventListener("click", () => {
   navbar.classList.toggle("change");
 });
+
+/* FEATURE DROP DOWN */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+  var filter, ul, li, a, i;
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+}
